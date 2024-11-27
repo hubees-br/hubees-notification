aaef47bbc98c5a1f116c2227872fa4a9b47ecc37
Marcio Sales <marcio@netpark.com.br>
2024-11-27 08:57:15 -0300

    fix: add AlarmManager

--- android/src/main/java/com/hubees/hubeesnotification/BeePassNotificationService.java
+++ android/src/main/java/com/hubees/hubeesnotification/BeePassNotificationService.java
@@ -1,5 +1,6 @@
 package com.hubees.hubeesnotification;
 
+import android.app.AlarmManager;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
@@ -7,8 +8,6 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
-import android.os.Looper;
-import android.os.Handler;
 import android.os.IBinder;
 import android.widget.RemoteViews;
 import androidx.core.app.NotificationCompat;
@@ -18,8 +17,6 @@ public class BeePassNotificationService extends android.app.Service {
     private static final int NOTIFICATION_ID = 1;
     private static final int STAY_TIME = 180;
 
-    private Handler handler;
-    private Runnable runnable;
     private int remainingTime;
     private int permanence;
     private String arrivalTime;
@@ -34,73 +31,89 @@ public class BeePassNotificationService extends android.app.Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        // Dados iniciais
-        remainingTime = intent.getIntExtra("remainingTime", 0);
-        permanence = intent.getIntExtra("permanence", 0);
-        arrivalTime = intent.getStringExtra("arrivalTime");
-        progress = calculateProgress(remainingTime);
+        if (intent != null) {
+            // Recupera os dados passados
+            remainingTime = intent.getIntExtra("remainingTime", 0);
+            permanence = intent.getIntExtra("permanence", 0);
+            arrivalTime = intent.getStringExtra("arrivalTime");
+            progress = calculateProgress(remainingTime);
 
-        startAutoUpdate();
-
-        // Configura a execução após 1 minuto
-        handler = new Handler(Looper.getMainLooper());
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-              startForeground(NOTIFICATION_ID, buildNotification());
+            // Atualiza a notificação
+            NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            if (manager != null) {
+                manager.notify(NOTIFICATION_ID, buildNotification());
             }
-        }, 500);
+
+            // Atualiza os valores para o próximo ciclo
+            if (!firstTime) {
+                remainingTime--;
+                permanence++;
+            }
+            firstTime = false;
+
+            // Agenda a próxima execução
+            scheduleNotificationUpdate();
+        }
 
         return START_STICKY;
     }
 
-    // @Override
-    // public void onTaskRemoved(Intent rootIntent) {
-    //     // Pare o serviço
-    //     stopSelf();
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        cancelNotificationUpdate();
+    }
 
-    //     // Cancele a notificação
-    //     NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-    //     if (manager != null) {
-    //         manager.cancel(1);
-    //     }
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
 
-    //     super.onTaskRemoved(rootIntent);
-    // }
+    private void scheduleNotificationUpdate() {
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
 
-    private void startAutoUpdate() {
-        handler = new Handler(Looper.getMainLooper());
-        runnable = new Runnable() {
-            @Override
-            public void run() {
-                if (!firstTime) {
-                  remainingTime--;
-                  permanence++;
-                  progress = calculateProgress(remainingTime);
-                }
+        Intent intent = new Intent(this, BeePassNotificationService.class);
+        intent.putExtra("remainingTime", remainingTime);
+        intent.putExtra("permanence", permanence);
+        intent.putExtra("arrivalTime", arrivalTime);
 
-                firstTime = false;
+        PendingIntent pendingIntent = PendingIntent.getService(
+                this,
+                0,
+                intent,
+                PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE
+        );
 
-                // Atualiza a notificação
-                NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-                manager.notify(NOTIFICATION_ID, buildNotification());
+        if (alarmManager != null) {
+            long triggerTime = System.currentTimeMillis() + 60000; // Dispara após 1 minuto
+            alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, triggerTime, pendingIntent);
+        }
+    }
 
-                // Reexecuta após 1 minuto
-                handler.postDelayed(this, 60000);
-            }
-        };
+    private void cancelNotificationUpdate() {
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
 
-        handler.post(runnable);
+        Intent intent = new Intent(this, BeePassNotificationService.class);
+        PendingIntent pendingIntent = PendingIntent.getService(
+                this,
+                0,
+                intent,
+                PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE
+        );
+
+        if (alarmManager != null) {
+            alarmManager.cancel(pendingIntent);
+        }
     }
 
     private Notification buildNotification() {
         Intent notificationIntent = getPackageManager().getLaunchIntentForPackage(getPackageName());
         notificationIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
         PendingIntent pendingIntent = PendingIntent.getActivity(
-            this,
-            0,
-            notificationIntent,
-            PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE
+                this,
+                0,
+                notificationIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE
         );
 
         if (remainingTime <= 0) {
@@ -115,18 +128,16 @@ public class BeePassNotificationService extends android.app.Service {
             notificationLayoutExcessLarge.setTextViewText(R.id.excess_time, formatTime(permanence - 180));
             notificationLayoutExcessLarge.setTextViewText(R.id.arrival_time, arrivalTime);
 
-            // Intent para fechar a notificação
             Intent closeIntent = new Intent(this, NotificationReceiver.class);
             closeIntent.setAction("CLOSE_NOTIFICATION");
             PendingIntent closePendingIntent = PendingIntent.getBroadcast(
-                this,
-                0,
-                closeIntent,
-                PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE
+                    this,
+                    0,
+                    closeIntent,
+                    PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE
             );
 
             notificationLayoutExcessLarge.setOnClickPendingIntent(R.id.button_close, closePendingIntent);
-
 
             return new NotificationCompat.Builder(this, CHANNEL_ID)
                     .setSmallIcon(R.drawable.hubees)
@@ -150,14 +161,13 @@ public class BeePassNotificationService extends android.app.Service {
             notificationLayoutLarge.setTextViewText(R.id.arrival_time_label, "Chegada " + arrivalTime);
             notificationLayoutLarge.setProgressBar(R.id.progressBar, 100, progress, false);
 
-            // Intent para fechar a notificação
             Intent closeIntent = new Intent(this, NotificationReceiver.class);
             closeIntent.setAction("CLOSE_NOTIFICATION");
             PendingIntent closePendingIntent = PendingIntent.getBroadcast(
-                this,
-                0,
-                closeIntent,
-                PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE
+                    this,
+                    0,
+                    closeIntent,
+                    PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE
             );
 
             notificationLayoutLarge.setOnClickPendingIntent(R.id.button_close, closePendingIntent);
@@ -200,18 +210,5 @@ public class BeePassNotificationService extends android.app.Service {
         int hours = minutes / 60;
         int mins = minutes % 60;
         return String.format("%02dh%02d", hours, mins);
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        if (handler != null) {
-            handler.removeCallbacks(runnable);
-        }
-    }
-
-    @Override
-    public IBinder onBind(Intent intent) {
-        return null;
     }
 }
