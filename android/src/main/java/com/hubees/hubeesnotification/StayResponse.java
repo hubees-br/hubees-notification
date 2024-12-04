97e92c91642f5ef5e6ad93a4bb4300c1509109eb
Marcio Sales <marcio@netpark.com.br>
2024-12-04 16:48:49 -0300

    fix: add package

--- android/src/main/java/com/hubees/hubeesnotification/StayResponse.java
+++ android/src/main/java/com/hubees/hubeesnotification/StayResponse.java
@@ -1,3 +1,5 @@
+package com.hubees.hubeesnotification;
+
 public class StayResponse {
     private int value;
 
--- android/src/main/java/com/hubees/hubeesnotification/StayService.java
+++ android/src/main/java/com/hubees/hubeesnotification/StayService.java
@@ -1,3 +1,5 @@
+package com.hubees.hubeesnotification;
+
 import retrofit2.Call;
 import retrofit2.http.GET;
 
